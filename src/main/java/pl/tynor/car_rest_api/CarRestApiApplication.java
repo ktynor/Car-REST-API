6d594329524569c4edad0e488d2318e34dd77797
ktynor <kt9@tlen.pl>
2022-02-15 19:49:43 +0000

    Enabling Swagger in the application

--- src/main/java/pl/tynor/car_rest_api/CarRestApiApplication.java
+++ src/main/java/pl/tynor/car_rest_api/CarRestApiApplication.java
@@ -9,5 +9,4 @@ public class CarRestApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(CarRestApiApplication.class, args);
     }
-
 }
---
+++ src/main/java/pl/tynor/car_rest_api/config/SwaggerConfiguration.java
@@ -0,0 +1,42 @@
+package pl.tynor.car_rest_api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+    @Bean
+    public Docket getDocket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .paths(PathSelectors.any())
+                .apis(RequestHandlerSelectors.any())
+                .build()
+                .apiInfo(apiInfo())
+                .securitySchemes(Collections.singletonList(apiKey()));
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Sig-Predict REST API Document")
+                .description("work in progress")
+                .termsOfServiceUrl("localhost")
+                .version("1.0")
+                .build();
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("jwtToken", "Authorization", "header");
+    }
+}
